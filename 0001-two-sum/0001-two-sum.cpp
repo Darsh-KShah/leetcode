ac1aa9f2e7349fdbb14e21b64621a7b61fa318e0
Darsh Shah <161367760+Darsh-KShah@users.noreply.github.com>
2026-08-07 12:27:06 +0530

    Time: 0 ms (100%), Space: 14.7 MB (57.27%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> um;
+        for(int i=0; i<nums.size(); i++) {
+            if(um.find(target - nums[i]) != um.end()) return {i, um[target - nums[i]]};
+            else um[nums[i]]=i;
+        }
+        return {-1, -1};
+    }
+};
