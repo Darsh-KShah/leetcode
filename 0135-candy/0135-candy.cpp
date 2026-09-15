16c9897bb77e76c41e12b783ff0e9ff233c6b55f
Darsh Shah <161367760+Darsh-KShah@users.noreply.github.com>
2026-09-15 23:12:17 +0530

    Time: 0 ms (100%), Space: 71.2 MB (36.54%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+
+        vector<int> v(n, 1);
+
+        for(int i = 1; i < n; i++)
+            if(ratings[i - 1] < ratings[i]) v[i] = v[i - 1] + 1;
+
+        int cnt = v[n - 1];
+
+        for(int i = n - 2; i >= 0; i--) {
+            if(ratings[i] > ratings[i + 1]) v[i] = max(v[i + 1] + 1, v[i]);
+
+            cnt += v[i];
+        }
+
+        return cnt;
+    }
+};
