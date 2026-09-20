55300bd4480f5ad8d8b9371291a707530701ebab
Darsh Shah <161367760+Darsh-KShah@users.noreply.github.com>
2026-09-20 12:47:04 +0530

    Time: 7 ms (8.31%), Space: 15.8 MB (62.61%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void sol(unordered_map<int, vector<pair<int, int>>> &mp, TreeNode* node, int row, int col, int &mini) {
+        if(node == NULL) return;
+
+        mini = min(mini, col);
+
+        mp[col].push_back({row, node->val});
+
+        sol(mp, node->left, row + 1, col - 1, mini);
+        sol(mp, node->right, row + 1, col + 1, mini);
+    }
+
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> res;
+
+        int mini = 0;
+
+        unordered_map<int, vector<pair<int, int>>> mp;
+
+        sol(mp, root, 0, 0, mini);
+
+        for(auto &i : mp) {
+            cout << i.first << ":";
+
+            for(auto &j : i.second) 
+                cout << j.first << " " << j.second << " ";
+
+            cout << endl;
+        }
+
+        cout << mini;
+
+        while(true) {
+            if(!mp.count(mini)) break;
+
+            int n = mp[mini].size();
+
+            sort(mp[mini].begin(), mp[mini].end());
+
+            vector<int> temp(n);
+
+            for(int i = 0; i < n; i++)
+                temp[i] = mp[mini][i].second;
+
+            res.push_back(temp);
+
+            mini++;
+        }
+
+        return res;
+    }
+};
